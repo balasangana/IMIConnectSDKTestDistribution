0e2b989a9dd8d7b9776d07e98485ab4400cd9fd6
Bala Obul Reddy Sangana <balaored@cisco.com>
2022-09-26 14:57:10 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IMIconnectCoreSDK",
+            targets: ["IMIconnectCoreSDK"])
+        ,
+        .library(
+            name: "IMIconnectNotificationServiceExtension",
             targets: ["IMIconnectNotificationServiceExtension"])
     ],
     dependencies: [
@@ -20,8 +24,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
        
         .binaryTarget(
+            name: "IMIconnectCoreSDK",
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/1.2/wxconnect.zip",
+            checksum: "659a6fef2c9c028b637755b3c1df1c2e035b3a90fac7920adb0083f3f0982e05"
+        ),
+        .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/1.1/wxconnect.zip",
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/1.2/wxconnect1.zip",
             checksum: "e67f518c3cc0c03c05cd67ccdb7cb7db145bb97cb0d97378db183444877ac5a7"
         )
     ]
