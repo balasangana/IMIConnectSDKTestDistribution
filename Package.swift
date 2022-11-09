13cd950f557ea6ff23d61e06638a116c789ae681
Bala Obul Reddy <balaored@cisco.com>
2022-11-09 14:30:15 +0530

    package.swift new

--- Package.swift
+++ Package.swift
@@ -1,12 +1,7 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
 let package = Package(
     name: "IMIconnectCoreSDK",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IMIconnectCoreSDK",
             targets: ["IMIconnectCoreSDK"])
@@ -15,25 +10,16 @@ let package = Package(
             name: "IMIconnectNotificationServiceExtension",
             targets: ["IMIconnectNotificationServiceExtension"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-       
         .binaryTarget(
             name: "IMIconnectCoreSDK",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/1.4.0/wxconnect3.zip",
-            checksum: "edef7f8e6a90aece32e04d8b0f30dba2f7928d09432923830644141183e2366c"
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0./wxConnectCoreSDK.zip",
+            checksum: "6820bb737e71cc508447828dc651ca22f5146070fd25a9d7c302949517c31b03"
         ),
         .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/1.4.0/wxconnect3.zip",
-            checksum: "edef7f8e6a90aece32e04d8b0f30dba2f7928d09432923830644141183e2366c"
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0./wxConnectNotificationServiceExtension.zip",
+            checksum: "e4964004e94b233c869a2c19e1fd974651b1b51fde1c2ed2d9796abe64747f5d"
         )
     ]
 )
-
-
