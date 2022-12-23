2713902f242339c99a9e10a9204f507dc9b5e2ab
Ashish Das <ashish.d@imimobile.com>
2022-12-23 11:53:11 +0000

    Updated Package.swift with v2.17.0.224

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
         .binaryTarget(
             name: "IMIconnectCoreSDK",
             url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.224/wxConnectCoreSDK.zip",
-            checksum: "e6754852703d30ac2fe2f0733cce63a4bb2e21c561f84334789150a1bbba7933"
+            checksum: "ee73a92200ade5471c9d6a1f7d32d5d2faddd05e37431122f3347cbcc4475617"
         ),
         .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
             url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.224/wxConnectNotificationServiceExtension.zip",
-            checksum: "45a8fb82b0b1ef1a5934b71e4c206cc8217efe7e6386b62016ca160ff46cfdb9"
+            checksum: "54296aac5724e8202d74ddf480b44f7849f52097d8bcfab128268616ca00dd2d"
         )
     ]
 )
