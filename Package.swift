f05d5a68921113175ba9f83627897ee19aca4a55
Bala Obul Reddy <balaored@cisco.com>
2023-01-05 19:56:55 +0530

    Updated Package.swift with v2.17.0.225

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ let package = Package(
         .library(
             name: "TestConnectPod",
             targets: ["IMIconnectCoreSDK"]),
-  
+#        .library(
+#            name: "TestConnectPod-Lite",
+#            targets: ["IMIconnectCoreSDK"]),
         .library(
             name: "IMIconnectNotificationServiceExtension",
             targets: ["IMIconnectNotificationServiceExtension"])
@@ -15,12 +17,17 @@ let package = Package(
         .binaryTarget(
             name: "IMIconnectCoreSDK",
             url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDK.zip",
-            checksum: "77b32ecaa0c690046642054377bbcffcf24344a42b8d8600e2bbe6b876213d25"
+            checksum: "30f7cab0a33d32885df828e1dceb0ab3a9caca69bff3ddb6193fa1f6531b208d"
         ),
+#        .binaryTarget(
+#            name: "IMIconnectCoreSDK",
+#            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDKLite.zip",
+#            checksum: "537930eb4f0fc44303b9747d0e807a396774bb83bf413c616d4bb67ed71a1492"
+#        ),
         .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
             url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectNotificationServiceExtension.zip",
-            checksum: "a11f9b9be7672e28afc75378b744ad7fd5bf247b7321682b2ad8abe619a84f3b"
+            checksum: "3b601cff66e4d9888165ba14dc7c39f19fa6cc60d367adc0413c530f70d49ee8"
         )
     ]
 )
