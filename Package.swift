3786df1a3546cc354049329857ba33b7ea306398
Bala Obul Reddy Sangana <112095774+balasangana@users.noreply.github.com>
2023-01-05 18:25:50 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ let package = Package(
         .library(
             name: "TestConnectPod",
             targets: ["IMIconnectCoreSDK"]),
-        .library(
-            name: "TestConnectPod-Lite",
-            targets: ["IMIconnectCoreSDK"]),
+  
         .library(
             name: "IMIconnectNotificationServiceExtension",
             targets: ["IMIconnectNotificationServiceExtension"])
@@ -18,11 +16,6 @@ let package = Package(
             name: "IMIconnectCoreSDK",
             url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDK.zip",
             checksum: "77b32ecaa0c690046642054377bbcffcf24344a42b8d8600e2bbe6b876213d25"
-        ),
-        .binaryTarget(
-            name: "IMIconnectCoreSDK",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDKLite.zip",
-            checksum: "c2f1db6a0f1b1e51f4ec7cea4f281777a232f6980d47fc03a77baf2eb6b8c92d"
         ),
         .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
