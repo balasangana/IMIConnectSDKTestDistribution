0f7bf52d540e686d21f7e23f967711ddc12bdc17
Bala Obul Reddy <balaored@cisco.com>
2023-01-06 16:07:37 +0530

    Updated Package.swift with v2.17.0.226

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ let package = Package(
         .library(
             name: "TestConnectPod",
             targets: ["IMIconnectCoreSDK"]),
-#        .library(
-#            name: "TestConnectPod-Lite",
-#            targets: ["IMIconnectCoreSDK"]),
         .library(
             name: "IMIconnectNotificationServiceExtension",
             targets: ["IMIconnectNotificationServiceExtension"])
@@ -16,18 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IMIconnectCoreSDK",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDK.zip",
-            checksum: "30f7cab0a33d32885df828e1dceb0ab3a9caca69bff3ddb6193fa1f6531b208d"
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.226/wxConnectCoreSDK.zip",
+            checksum: "52e08f0e76985e48f86d2cc9abfe996097441fd0866923b562b731d5769c6726"
         ),
-#        .binaryTarget(
-#            name: "IMIconnectCoreSDK",
-#            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectCoreSDKLite.zip",
-#            checksum: "537930eb4f0fc44303b9747d0e807a396774bb83bf413c616d4bb67ed71a1492"
-#        ),
         .binaryTarget(
             name: "IMIconnectNotificationServiceExtension",
-            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.225/wxConnectNotificationServiceExtension.zip",
-            checksum: "3b601cff66e4d9888165ba14dc7c39f19fa6cc60d367adc0413c530f70d49ee8"
+            url: "https://github.com/balasangana/IMIConnectSDKTestDistribution/releases/download/2.17.0.226/wxConnectNotificationServiceExtension.zip",
+            checksum: "40359888df6f0f102aafc649e53527d7c542898cbb78287933822ba4cdaf9951"
         )
     ]
 )
